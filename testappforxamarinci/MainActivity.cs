ec6df9a21fd865d843b152807f20a98bf59310bd
joshuang2 <joshuang2@datacom.co.nz>
2015-10-15 11:57:11 +1300

    Button Press Text Change

--- testappforxamarinci/MainActivity.cs
+++ testappforxamarinci/MainActivity.cs
@@ -26,7 +26,7 @@ namespace testappforxamarinci
 			Button button = FindViewById<Button> (Resource.Id.myButton);
 			
 			button.Click += delegate {
-				button.Text = string.Format ("{0} clicks!", count++);
+				button.Text = string.Format ("{0} button presses!", count++);
 			};
 		}
 	}
